625e97f625c1c0a17416a8122d4a20ccae9c14e7
Krishna <tshakshi54@gmail.com>
2023-02-07 19:36:28 +0530

    EvaluateReversePolishNotation.java added in Microsoft

---
+++ Microsoft/EvaluateReversePolishNotation.java
@@ -0,0 +1,31 @@
+
+import java.util.*;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for(String ch: tokens){
+            if(ch.equals("+")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(a+b);
+            } else if(ch.equals("-")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b-a);
+            } else if(ch.equals("*")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(a*b);
+            } else if(ch.equals("/")){
+                int a = stack.pop();
+                int b = stack.pop();
+                stack.push(b/a);
+            } else {
+                stack.push(Integer.parseInt(ch));
+            }
+        }
+        return stack.pop();
+    }
+}
